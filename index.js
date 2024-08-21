4e055c242e7d93e5cf08e83b9dfcee3d8bfaee20
umransozeri <umransozeri954qr3@gmail.com>
2024-08-21 05:47:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("sosa is live");
+const urlParams = new URLSearchParams(window.location.search);
+
+
+if (urlParams.get('btag') === 'tony11') {
+   const registerButton = document.querySelector('.register');
+
+
+    if (registerButton) {
+        registerButton.click();
+    }
+}
