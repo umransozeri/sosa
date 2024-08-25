3c91fe4c8eeafc5158cff4d519c26812ad1ff80b
umransozeri <umransozeri954qr3@gmail.com>
2024-08-25 15:15:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
+  var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Güncel sürüm
+    script.onload = function() { // jQuery yüklendikten sonra çalışacak kod
+        $(document).ready(function() {
+            function getQueryParam(param) {
+                let urlParams = new URLSearchParams(window.location.search);
+                return urlParams.get(param);
+            }
 
-$(document).ready(function() {
-    function getQueryParam(param) {
-        let urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get(param);
-    }
+            let btag = getQueryParam('btag');
 
-    let btag = getQueryParam('btag');
+            if (btag === 'tony11') {
+                if (!sessionStorage.getItem('btagnew')) {
+                    sessionStorage.setItem('btagnew', btag);
+                }
 
-    if (btag === 'tony11') {
-        if (!sessionStorage.getItem('btagnew')) {
-            sessionStorage.setItem('btagnew', btag);
-        }
-
-        if (sessionStorage.getItem('btagnew')) {
-            setTimeout(function() {
-                $('.register').trigger('click');
-            }, 500); 
-            sessionStorage.removeItem('btagnew');
-        }
-    }
-});
+                if (sessionStorage.getItem('btagnew')) {
+                    setTimeout(function() {
+                        $('.register').trigger('click');
+                    }, 500);
+                    sessionStorage.removeItem('btagnew');
+                }
+            }
+        });
+    };
+    document.getElementsByTagName('head')[0].appendChild(script);
