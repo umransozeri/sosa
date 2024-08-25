5e99fee684f11dd40e555953cdecae69daa4a81b
umransozeri <umransozeri954qr3@gmail.com>
2024-08-25 15:10:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-import { useEffect } from 'react';
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
 
-function MyComponent() {
-  useEffect(() => {
-    const btag = sessionStorage.getItem('btag');
+$(document).ready(function() {
+    function getQueryParam(param) {
+        let urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(param);
+    }
+
+    let btag = getQueryParam('btag');
 
     if (btag === 'tony11') {
-      setTimeout(() => {
-        const registerButton = document.querySelector('.register');
-
-        if (registerButton) {
-          registerButton.click();
+        if (!sessionStorage.getItem('btagnew')) {
+            sessionStorage.setItem('btagnew', btag);
         }
-      }, 4000);
+
+        if (sessionStorage.getItem('btagnew')) {
+            setTimeout(function() {
+                $('.register').trigger('click');
+            }, 500); 
+            sessionStorage.removeItem('btagnew');
+        }
     }
-    
-    return () => {
-      sessionStorage.removeItem('btag');
-    };
-  }, []);
-
-  return (
-    <div>
-    
-    </div>
-  );
-}
-
-export default MyComponent;
+});
